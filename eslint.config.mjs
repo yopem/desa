f94a405d6e8c5d36d9f6345b5015a9b03ba12250
Karyana Yandi <karyana@yandi.me>
2025-06-07 12:58:15 +0700

    chore(lint): disable no-empty-object-type rule
    Disable @typescript-eslint/no-empty-object-type rule to allow empty object
    type declarations in TypeScript code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
   {
     rules: {
       "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
       "no-prototype-builtins": "off",
     },
   },
